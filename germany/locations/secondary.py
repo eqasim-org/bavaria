0d5854057dee709cd00ba5b51e22a6eed490b01b
Sebastian Hörl <hoerl.sebastian@gmail.com>
2024-07-20 01:08:23 +0200

    integrate osm and education separation

--- germany/locations/secondary.py
+++ germany/locations/secondary.py
@@ -6,16 +6,25 @@ Yield work location candidates for Germany.
 """
 
 def configure(context):
-    context.stage("germany.data.buildings")
+    context.stage("germany.data.osm.locations")
 
 def execute(context):
     # Load data
-    df = context.stage("germany.data.buildings")
+    df = context.stage("germany.data.osm.locations")
 
-    df["location_id"] = "sec_" + df["building_id"].astype(str)
-    df["offers_leisure"] = True
-    df["offers_shop"] = True
+    # Activity types    
+    df["offers_leisure"] = df["location_type"] == "leisure"
+    df["offers_shop"] = df["location_type"] == "shop"
     df["offers_other"] = True
+
+    # Filter
+    df = df[
+        df["offers_leisure"] | df["offers_shop"] | df["offers_other"]
+    ].copy()
+
+    # Identifiers
+    df["location_id"] = np.arange(len(df))
+    df["location_id"] = "sec_" + df["location_id"].astype(str)
 
     return df[[
         "location_id", "commune_id", "iris_id", "geometry",
