e367a9571ed95640ddc17b06abe3c824d3826928
Sebastian Hörl <hoerl.sebastian@gmail.com>
2025-03-14 12:54:38 +0100

    issue with path names

--- bavaria/data/population/raw.py
+++ bavaria/data/population/raw.py
@@ -10,14 +10,14 @@ This stages loads a file containing population data for Germany including the ad
 def configure(context):
     context.config("data_path")
     context.config("bavaria.political_prefix", ["091", "092", "093", "094", "095", "096", "097"])
-    context.config("bavaria.population_path", "germany/vg250-ew_12-31.utm32s.gpkg.ebenen.zip")
-    context.config("bavaria.population_source", "vg250-ew_12-31.utm32s.gpkg.ebenen/vg250-ew_ebenen_1231/DE_VG250.gpkg")
+    context.config("germany.population_path", "germany/vg250-ew_12-31.utm32s.gpkg.ebenen.zip")
+    context.config("germany.population_source", "vg250-ew_12-31.utm32s.gpkg.ebenen/vg250-ew_ebenen_1231/DE_VG250.gpkg")
 
 def execute(context):
     # Load IRIS registry
     with zipfile.ZipFile(
-        "{}/{}".format(context.config("data_path"), context.config("bavaria.population_path"))) as archive:
-        with archive.open(context.config("bavaria.population_source")) as f:
+        "{}/{}".format(context.config("data_path"), context.config("germany.population_path"))) as archive:
+        with archive.open(context.config("germany.population_source")) as f:
             df_population = gpd.read_file(f, layer = "vg250_gem")[[
                 "ARS", "EWZ", "geometry"
             ]]
@@ -44,7 +44,7 @@ def execute(context):
     return df_population
 
 def validate(context):
-    if not os.path.exists("%s/%s" % (context.config("data_path"), context.config("bavaria.population_path"))):
+    if not os.path.exists("%s/%s" % (context.config("data_path"), context.config("germany.population_path"))):
         raise RuntimeError("German population data is not available")
 
-    return os.path.getsize("%s/%s" % (context.config("data_path"), context.config("bavaria.population_path")))
+    return os.path.getsize("%s/%s" % (context.config("data_path"), context.config("germany.population_path")))
